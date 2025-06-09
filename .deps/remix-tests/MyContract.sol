c1582e79aa79da4dc3e9db863aabb86bc68e886b
MaxTurner0126 <luckywatermelon1006@gmail.com>
2025-06-09 17:33:20 +0900

    Start of Soildity_Course

---
+++ .deps/remix-tests/MyContract.sol
@@ -0,0 +1,10 @@
+// Solidity program to create 
+// Hello World program
+pragma solidity ^0.8.3;
+
+contract MyContract 
+{
+  address public newAddr = 0x9999999999999999999999999999999999999999;
+  int public neg = -1;
+  uint8 public newU = 0;
+}
